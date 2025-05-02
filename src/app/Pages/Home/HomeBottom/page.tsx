01f7deb2f72422b725b7f28d909af40a2e001451
Surajjberaa <berasuraj08@gmail.com>
2025-05-02 17:11:24 +0530

    Added Lenis

--- src/app/Pages/Home/HomeBottom/page.tsx
+++ src/app/Pages/Home/HomeBottom/page.tsx
@@ -30,7 +30,7 @@ function HomeBottom() {
         isMobile ? <img className='object-cover absolute -z-10  h-full w-full ' src="https://res.cloudinary.com/dggyfyynb/image/upload/v1746183512/minh-pham-OtXADkUh3-I-unsplash_f5rctu.jpg" alt="" /> : <img className='object-cover absolute -z-10  h-full w-full' src="https://res.cloudinary.com/dggyfyynb/image/upload/v1746183512/spacejoy-85pCvDWDMmI-unsplash_vccdkc.jpg" alt="" />
       }
 
-      <div className='flex md:flex-row flex-col justify-between items-end text-center text-black md:h-[30vh] md:pt-0 pt-20 bg-white/10 backdrop-blur-[2px] rounded-b-xl  w-[80%] h-[50vh]'>
+      <div className='flex md:flex-row flex-col justify-between items-end text-center text-black md:h-[30vh] md:pt-0 pt-20 bg-white/10 backdrop-blur-[2px] md:rounded-t-xl rounded-b-xl  w-[80%] h-[50vh]'>
         <div className='md:flex-3/4 flex-col items-start justify-start p-10 h-full'>
           <h1 className='md:text-7xl text-4xl font-light items-start md:text-start'>Stay Close to the Craft</h1>
           <p className='md:text-lg text-sm font-extralight items-start md:text-start mt-10'>Join Ferny Circle and get 10% off all full-price furniture. Enjoy exclusive early access to new collections, additional 15% off selected sale items, and free delivery on every other - all year round. Plus, receive styling tips and member-only surprises.</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
+'use client'
 import Image from "next/image";
 import Landing from "./pages/home/Lanfing/page";
 import Home from "./pages/home/page";
+import Lenis from "lenis";
+import { ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
+import ReactLenis, { useLenis } from "lenis/react";
+
 
 export default function Root() {
+
+  //Lenis
+  const lenis = useLenis(({ scroll }) => {
+    // called every scroll
+  });
+  
+
   return (
     <>
+    <ReactLenis root>
       <Home />
+    </ReactLenis>
     </>
   );
 }
